cf7613e125113edcc444c651b922727f690485b2
lovetocode <dshaggy28@yahoo.com>
2025-01-18 17:45:08 -0800

    worked on state machine

--- divasofdestruction/Players/Scripts/StateMachine.gd
+++ divasofdestruction/Players/Scripts/StateMachine.gd
@@ -1,12 +1,26 @@
 class_name  State_machine
-extends state
+extends State
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var current_state: State
 
+@export var starting_state: State
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func init() -> void: change_state(starting_state)
+
+func process_frame(delta: float) -> void:
+	var new_state: State = current_state.process_frame(delta)
+	if new_state: change_state(new_state)
+
+func proces_input(event: InputEvent) -> void:
+	var new_state: State = current_state.process_frame(event)
+	if new_state: change_state(new_state)
+	
+func process_physics(delta: float) -> void:
+	var new_state: State = current_state.process_physics(delta)
+	if new_state: change_state(new_state)
+	
+func change_state(new_state: State) -> void:
+	current_state.exit()
+	current_state = new_state
+	current_state.enter()
